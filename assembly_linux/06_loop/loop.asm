ef79d66a6660ccffd38f363f236fb281583c195c
Fábio Adler <adlerluna@hotmail.com>
2026-05-03 17:34:26 -0300

    Aprendendo a fazer um loop com condição

---
+++ assembly_linux/06_loop/loop.asm
@@ -0,0 +1,48 @@
+section .data
+    msg1 db "Começando...",0xA,0x0
+    len1 equ $- msg1
+
+section .bss
+    num resb 0x3
+
+section .text
+    global _start
+
+_start:
+    mov ecx,msg1
+    mov edx,len1
+    call .print
+
+    ;Vamos fazer um loop semelhante ao for de 0 in 9
+    mov esi,0x0
+    mov edi,0x9
+    call .loop
+
+    mov eax,0x1
+    mov ebx,0x0
+    int 0x80
+
+.loop:
+    mov eax,esi                 ;carreganos o valor de esi em eax, para não alterarmos o valor de esi 
+    add eax,'0'                 ;Adicionamos um '0' dessa forma, para convertermos para str
+    mov [num],eax               ;Carregamos o conteudo de eax no endereço de memoria que reservamos para num
+    mov byte [num+1],0xA        ;Adicionamos mais um caracter 1 byte a frente 0xA = 10, que siginifica uma quebra de linha ou \n
+    mov byte [num+2],0x0        ;Adicionamos um caracter na frente do ultimo que adicionamos acima, ou seja pegamo o endereo atual e colocamos +2 que são 2 bytes a frente, adicionamos um 0x0 = 0 que siginifica que a string acabou
+
+    mov ecx,num                 ;Carregamos a mensagem para ser escrita
+    mov edx,0x3                 ;Colocamos o tamanho de 0x3 = 3 bytes, por que e o numero e os outros 2 caracteres que Adicionamos
+    call .print                 ;Chamamos a nossa função de print
+
+    cmp esi,edi                 ;Confere se o valor de esi atual e igual ao de edi que passamos antés call para chamar a funão
+    je .fim_loop                ;Se a confição for verdadeira pular para o .fim_loop
+    inc esi                     ;Incrementa esi com +1
+    jmp .loop                   ;Volta para o loop novamente
+
+.fim_loop:
+    ret
+    
+.print:
+    mov eax,0x4
+    mov ebx,0x1
+    int 0x80
+    ret
